254f342c8cdd87a88bcf595fc338b75fa44097ec
MarvinBaylon07 <147330296+MarvinBaylon07@users.noreply.github.com>
2025-03-18 05:40:59 -0500

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,66 +55,63 @@ $(function () {
         opacity: 1,
     });
 
+    // Show Animation 1
     timeline.fromTo(
-        ".mil-animation-1 .mil-h3", {
-            y: "30px",
-            opacity: 0
-        }, {
-            y: "0px",
-            opacity: 1,
-            stagger: 0.4
-        },
+        ".mil-animation-1 .mil-h3",
+        { y: "30px", opacity: 0 },
+        { y: "0px", opacity: 1, stagger: 1 }
     );
 
+    // Fade out Animation 1 before showing Animation 2
     timeline.to(".mil-animation-1 .mil-h3", {
         opacity: 0,
-        y: '-30',
-    }, "+=.3");
+        y: "-30",
+    }, "+=1");
 
-    timeline.fromTo(".mil-reveal-box", 0.1, {
-        opacity: 0,
-    }, {
-        opacity: 1,
-        x: '-30',
-    });
+    // Show Animation 2
+    timeline.fromTo(
+        ".mil-animation-2 .mil-h3",
+        { y: "30px", opacity: 0 },
+        { y: "0px", opacity: 1, stagger: 1 }
+    );
 
-    timeline.to(".mil-reveal-box", 0.45, {
-        width: "100%",
-        x: 0,
-    }, "+=.1");
-    timeline.to(".mil-reveal-box", {
-        right: "0"
-    });
-    timeline.to(".mil-reveal-box", 0.3, {
-        width: "0%"
-    });
-    timeline.fromTo(".mil-animation-2 .mil-h3", {
+    // Fade out Animation 2 before showing Animation 3
+    timeline.to(".mil-animation-2 .mil-h3", {
         opacity: 0,
-    }, {
-        opacity: 1,
-    }, "-=.5");
-    timeline.to(".mil-animation-2 .mil-h3", 0.6, {
+        y: "-30",
+    }, "+=1");
+
+    // Show Animation 3
+    timeline.fromTo(
+        ".mil-animation-3 .mil-h3",
+        { y: "30px", opacity: 0 },
+        { y: "0px", opacity: 1, stagger: 1 }
+    );
+
+    // Fade out Animation 3
+    timeline.to(".mil-animation-3 .mil-h3", 0.6, {
         opacity: 0,
-        y: '-30'
+        y: "-30"
     }, "+=.5");
-    timeline.to(".mil-preloader", 0.8, {
-        opacity: 0,
-        ease: 'sine',
-    }, "+=.2");
-    timeline.fromTo(".mil-up", 0.8, {
-        opacity: 0,
-        y: 40,
-        scale: .98,
-        ease: 'sine',
 
-    }, {
-        y: 0,
-        opacity: 1,
-        scale: 1,
-        onComplete: function () {
-            $('.mil-preloader').addClass("mil-hidden");
+    // Fade out preloader and show the main content
+    timeline.to(".mil-preloader", 0.8, { opacity: 0, ease: "sine" }, "+=.2");
+
+    timeline.fromTo(
+        ".mil-up",
+        0.8,
+        { opacity: 0, y: 40, scale: 0.98, ease: "sine" },
+        {
+            y: 0,
+            opacity: 1,
+            scale: 1,
+            onComplete: function () {
+                $(".mil-preloader").addClass("mil-hidden");
+            }
         },
-    }, "-=1");
+        "-=1"
+    );
+
     /***************************
 
     anchor scroll
